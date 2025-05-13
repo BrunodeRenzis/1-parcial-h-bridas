8aaddef1012eddaf28972ef3451322b20f70c949
Bruno de Renzis <brunoderenzisrubinetti26@gmail.com>
2025-05-13 09:23:21 -0300

    Incluyo cors

--- src/app.js
+++ src/app.js
@@ -3,10 +3,11 @@ import frasesRoutes from './routes/frases.routes.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import personajesRoutes from './routes/personajes.routes.js';
-
+import cors from 'cors';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -15,9 +16,9 @@ app.get('/', (req, res) => {
     <h1>API Rick & Morty Frases</h1>
     <p>Consulta los endpoints /api/frases y /api/personajes</p>
     <footer>
-      Nombre: Tu Nombre <br />
+      Nombre: Bruno de Renzis<br />
       Materia: Aplicaciones Híbridas <br />
-      Docente: Nombre del docente <br />
+      Docente: Marcos Galvan Camila Belén <br />
       Comisión: X
     </footer>
   `);
